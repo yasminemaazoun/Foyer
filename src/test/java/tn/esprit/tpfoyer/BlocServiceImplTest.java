8ca6c7eaeb2cb4fbec8155748ccb7aff9d3ac789
yasminemaazoun <yasmine.maazoun@esprit.tn>
2024-11-04 21:00:52 +0100

    t

--- src/test/java/tn/esprit/tpfoyer/BlocServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/BlocServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
-class BlocServiceImplTestCase {
+class BlocServiceImplTest { // Renamed here
     @Mock
     BlocRepository blocRepository;
 
